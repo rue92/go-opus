0216841506e2703179b1e8091963833e6aed6bdf
Andrew Shugarts <rue92@vt.edu>
2016-03-22 23:52:46 -0400

    Port opus.c to Go more natively
    My goal here is to basically delete all the naive
    porting I did before as it neither takes advantage of Go's
    features, nor is it clean readable code. Since nothing works
    anyway, I've done a bunch of messing around to make sure
    that syntactically speaking, the code builds, but that
    doesn't actually mean anything at this point. It doesn't work.
    And it won't work for quite some time at this rate.

--- mlp.go
+++ mlp.go
@@ -1,21 +1,21 @@
 package opus
 
 type MLP struct {
-	layers int32,
-	topo const *int32,
-	weights const *int32
-};
+	layers int32
+	topo []int32
+	weights []int32
+}
 
-func mlp_process(m *MLP, in, out *float) {
+func mlp_process(m *MLP, in, out *float32) {
 
 	var j int32
-	var hidden int16[MAX_NEURONS]
-	var W := m.weights
-	var W_index := 0
+//	var hidden int16//[MAX_NEURONS]
+//	W := m.weights
+	W_index := 0
 
 	for j = 0; j < m.topo[1]; j++ {
-		var k int32
-		var sum = W[W_index]
+//		var k int32
+//		var sum = W[W_index]
 		W_index++
 		
 	}
